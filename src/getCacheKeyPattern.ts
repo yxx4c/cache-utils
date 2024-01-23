6d7bd73eec262892aac971afb48845753abab092
caxxy <yxx4c@users.noreply.github.com>
2024-01-24 00:23:08 +0300

    feat(cacheKey): add glob check and custom delimiter and case options

--- src/getCacheKeyPattern.ts
+++ src/getCacheKeyPattern.ts
@@ -1,15 +1,33 @@
-import {camelCase} from 'lodash';
+import { CacheKeyPatternParams } from "./types";
+import { CacheCase, caseMap } from "./utils";
 
-export type CacheKeyPatternParams = {[key: string]: string}[];
+const globCheckRegex = /[*?]/;
 
-export const getCacheKeyPattern = (params: CacheKeyPatternParams) =>
+const globCheck = (s: string) => globCheckRegex.test(s);
+
+export const getCacheKeyPattern = (
+  params: CacheKeyPatternParams,
+  delimiter = ":",
+  cacheCase: CacheCase = CacheCase.CAMEL_CASE
+) =>
   params
-    .map(obj =>
-      Object.entries(obj).map(([key, value]) =>
-        key.toLocaleLowerCase() === 'glob' ? value : `${camelCase(key)}:${camelCase(value)}`,
-      ),
+    .map((obj) =>
+      Object.entries(obj)
+        .map(([key, value]) => {
+          if (key.toLowerCase() === "glob") return value;
+
+          const formattedKey = globCheck(key)
+            ? key
+            : caseMap[cacheCase](key);
+
+          const formattedValue = globCheck(value)
+            ? value
+            : caseMap[cacheCase](value);
+
+          return `${formattedKey}:${formattedValue}`;
+        })
+        .join(delimiter)
     )
-    .join(':');
+    .join(delimiter);
 
-
-export default getCacheKeyPattern
+export default getCacheKeyPattern;
